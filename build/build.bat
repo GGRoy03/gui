7eca8d68271e2efac83e84b3297063eb583e141b
Guillaume Roy <royguillaume2005@icloud.com>
2025-12-18 15:24:56 -0500

    chore: sample build file

---
+++ build/build.bat
@@ -0,0 +1,49 @@
+@echo off
+SETLOCAL
+
+:: ------------------------------------
+:: Config
+:: ------------------------------------
+set "CXX=clang-cl"
+
+set "TARGET=void_engine.exe"
+set "SRCS=..\incremental_build.cpp"
+set "INCLUDES=..\..\ui_engine"
+set "OUT_DIR=."
+set "PDBNAME=void_engine.pdb"
+
+:: ------------------------------------
+:: Build type
+:: ------------------------------------
+if /I "%~1"=="release" (
+    set "TARGET=void_engine_release.exe"
+    set "BUILD=release"
+    set "CXXFLAGS=/O2 -maes -Wno-deprecated-declarations /std:c++20 /EHsc"
+    set "LDFLAGS="
+) else (
+    set "BUILD=debug"
+    set "CXXFLAGS=/Od /Zi /W3 -maes -Wno-unused-function -Wno-deprecated-declarations /std:c++20 /EHsc /DDEBUG"
+    set "LDFLAGS="
+)
+
+echo Building %TARGET% (%BUILD%)...
+echo CXXFLAGS: %CXXFLAGS%
+echo.
+
+:: ------------------------------------
+:: One-step compile + link
+:: ------------------------------------
+"%CXX%" %SRCS% ^
+    /I "%INCLUDES%" ^
+    %CXXFLAGS% ^
+    /Fe"%OUT_DIR%\%TARGET%" ^
+    /link %LDFLAGS% /DEBUG /PDB:"%OUT_DIR%\%PDBNAME%"
+
+if errorlevel 1 (
+    echo *** Build failed ***
+    exit /b 1
+)
+
+echo *** BUILD SUCCEEDED: %OUT_DIR%\%TARGET% ***
+ENDLOCAL
+exit /b 0

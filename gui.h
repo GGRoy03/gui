5a5497f6f69b4cfa730da6eee7fc934abc651e3b
Guillaume Roy <royguillaume2005@icloud.com>
2026-01-11 16:32:49 -0500

    feat: Update to how we set the style for each node

--- gui.h
+++ gui.h
@@ -248,7 +248,7 @@ GUI_API uint32_t GuiCreateNode   (uint64_t Key, uint32_t Flags, gui_layout_tree 
 GUI_API void     GuiUpdateLayout (uint32_t NodeIndex, gui_layout_properties *Properties, gui_layout_tree *Tree);
 
 
-GUI_API void     GuiEnterParent  (uint32_t NodeIndex, gui_layout_tree *Tree, gui_parent_node *Node);
+GUI_API gui_bool GuiEnterParent  (uint32_t NodeIndex, gui_layout_tree *Tree, gui_parent_node *Node);
 GUI_API void     GuiLeaveParent  (uint32_t NodeIndex, gui_layout_tree *Tree);
 
 
@@ -355,6 +355,8 @@ typedef struct gui_render_command_list
     uint32_t            Count;
 } gui_render_command_list;
 
+GUI_API void GuiUpdateStyle  (uint32_t NodeIndex, gui_paint_properties *Properties, gui_layout_tree *Tree);
+
 
 GUI_API gui_memory_footprint    GuiGetRenderCommandsFootprint  (gui_layout_tree *Tree);
 GUI_API gui_render_command_list GuiComputeRenderCommands       (gui_layout_tree *Tree, gui_memory_block Block);
@@ -372,7 +374,6 @@ GUI_API gui_render_command_list GuiComputeRenderCommands       (gui_layout_tree 
 GUI_API void GuiBeginFrame  (gui_pointer_event_list *EventList, gui_layout_tree *Tree);
 GUI_API void GuiEndFrame    (void);
 
-#define GUI_IMPLEMENTATION
 #ifdef GUI_IMPLEMENTATION
 
 
@@ -1761,7 +1762,7 @@ GuiUpdateLayout(uint32_t NodeIndex, gui_layout_properties *Properties, gui_layou
 }
 
 
-GUI_API void
+GUI_API gui_bool
 GuiEnterParent(uint32_t NodeIndex, gui_layout_tree *Tree, gui_parent_node *ParentNode)
 {
     if(GuiIsValidLayoutTree(Tree) && ParentNode)
@@ -1771,6 +1772,8 @@ GuiEnterParent(uint32_t NodeIndex, gui_layout_tree *Tree, gui_parent_node *Paren
 
         Tree->Parent = ParentNode;
     }
+
+    return GUI_TRUE;
 }
 
 
@@ -1846,6 +1849,128 @@ GuiComputeTreeLayout(gui_layout_tree *Tree)
 
         GuiPlaceLayout(ActiveRoot, Tree, 0);
     }
+}
+
+// =================================================
+// @PRIVATE IMPLEMENTATION
+//
+// <Painting>
+// =================================================
+
+#define GUI_MAX_COMMAND_PER_NODE 2u
+
+
+GUI_API void
+GuiUpdateStyle(uint32_t NodeIndex, gui_paint_properties *Properties, gui_layout_tree *Tree)
+{
+    if (GuiIsValidLayoutTree(Tree) && Properties)
+    {
+        memcpy(&Tree->PaintBuffer[NodeIndex], Properties, sizeof(gui_paint_properties));
+    }
+}
+
+
+GUI_API gui_memory_footprint
+GuiGetRenderCommandsFootprint(gui_layout_tree *Tree)
+{
+    gui_memory_footprint Result;
+    Result.SizeInBytes = 0;
+    Result.Alignment   = 0;
+
+    if (GuiIsValidLayoutTree(Tree))
+    {
+        uint64_t CommandSize = Tree->NodeCount * GUI_MAX_COMMAND_PER_NODE * sizeof(gui_render_command);
+        uint64_t QueueSize   = Tree->NodeCount * sizeof(uint32_t);
+
+        Result.SizeInBytes = CommandSize + QueueSize;
+        Result.Alignment   = GUI_ALIGN_OF(gui_render_command);
+    }
+
+    return Result;
+}
+
+
+GUI_API gui_render_command_list
+GuiComputeRenderCommands(gui_layout_tree *Tree, gui_memory_block Block)
+{
+    gui_render_command_list Result;
+    Result.Commands = NULL;
+    Result.Count    = 0;
+
+    gui_memory_region Local = GuiEnterMemoryRegion(Block);
+
+    if (GuiIsValidLayoutTree(Tree) && GuiIsValidMemoryRegion(&Local))
+    {
+        Result.Commands = GuiPushArray(&Local, gui_render_command, Tree->NodeCount * GUI_MAX_COMMAND_PER_NODE);
+        Result.Count    = 0;
+
+        uint32_t *NodeQueue = GuiPushArray(&Local, uint32_t, Tree->NodeCount);
+        uint32_t  Head      = 0;
+        uint32_t  Tail      = 0;
+
+        NodeQueue[Tail++] = (uint32_t)Tree->RootIndex;
+
+        while (Head < Tail)
+        {
+            uint32_t         NodeIndex = NodeQueue[Head++];
+            gui_layout_node *Node      = GuiGetLayoutNode(NodeIndex, Tree);
+            GUI_ASSERT(Node);
+
+            for (uint32_t Child = Node->First; Child != GuiInvalidIndex; Child = GuiGetLayoutNode(Child, Tree)->Next)
+            {
+                NodeQueue[Tail++] = Child;
+            }
+
+            gui_paint_properties *Paint = &Tree->PaintBuffer[Node->Index];
+
+            gui_color         Color        = Paint->Color;
+            gui_color         BorderColor  = Paint->BorderColor;
+            float             BorderWidth  = Paint->BorderWidth;
+            gui_corner_radius CornerRadius = Paint->CornerRadius;
+
+            if (Node->State & Gui_NodeState_UseFocusedStyle)
+            {
+                Color        = Paint->FocusedColor;
+                BorderColor  = Paint->FocusedBorderColor;
+                BorderWidth  = Paint->FocusedBorderWidth;
+                CornerRadius = Paint->FocusedCornerRadius;
+            }
+            else if (Node->State & Gui_NodeState_UseHoveredStyle)
+            {
+                Color        = Paint->HoveredColor;
+                BorderColor  = Paint->HoveredBorderColor;
+                BorderWidth  = Paint->HoveredBorderWidth;
+                CornerRadius = Paint->HoveredCornerRadius;
+
+                Node->State = (Node->State & ~Gui_NodeState_UseHoveredStyle);
+            }
+
+            if (Color.A > 0.0f)
+            {
+                gui_render_command *Command = &Result.Commands[Result.Count++];
+
+                Command->Type = Gui_RenderCommandType_Rectangle;
+                Command->Box  = GuiGetLayoutNodeBoundingBox(Node);
+
+                Command->Rect.Color        = Color;
+                Command->Rect.CornerRadius = CornerRadius;
+            }
+
+            if (BorderWidth > 0.0f)
+            {
+                gui_render_command *Command = &Result.Commands[Result.Count++];
+
+                Command->Type = Gui_RenderCommandType_Border;
+                Command->Box  = GuiGetLayoutNodeBoundingBox(Node);
+
+                Command->Border.Color        = BorderColor;
+                Command->Border.CornerRadius = CornerRadius;
+                Command->Border.Width        = BorderWidth;
+            }
+        }
+    }
+
+    return Result;
 }
 
 // =================================================
